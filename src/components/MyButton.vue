4d8e8e219e5e7788b812699bcac7cf511e127ce5
Neill Robson <neill.robson@pendo.io>
2024-03-29 15:05:32 -0400

    functional component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <pendo-button :label="`Count: ${count}`" @click="count++" />
+</template>
+
+<script>
+import { PendoButton } from '@pendo/components';
+
+export default {
+    components: {
+        PendoButton
+    },
+    data() {
+        return {
+            count: 0
+        };
+    }
+};
+</script>
