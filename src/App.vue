3736ce2f344771b433cd14ed7146821a5504feab
Neill Robson <neill.robson@pendo.io>
2024-05-17 15:43:32 -0400

    Basic CSS scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,83 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-        <pendo-button label="Click me" />
-        <my-markdown />
+        <header><strong>Page Header</strong></header>
+        <nav>
+            Sidebar Navigation
+            <ul>
+                <li>Overview</li>
+                <li>Elements</li>
+                <li>Guidelines</li>
+                <li>Components</li>
+                <li>Patterns</li>
+            </ul>
+        </nav>
+        <main>
+            <MyMarkdown class="content" />
+        </main>
+        <footer><em>Page Footer</em></footer>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import MyMarkdown from './components/MyMarkdown.mdx';
-import { PendoButton } from '@pendo/components';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld,
-        MyMarkdown,
-        PendoButton
+        MyMarkdown
     }
 };
 </script>
 
-<style>
+<style scoped>
 @import '@pendo/components/lib/es/components.css';
 
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    height: 100vh;
+    display: grid;
+    grid-template: auto 1fr auto / auto 1fr;
+    grid-template-areas: 'header header' 'nav main' 'footer footer';
     color: #2c3e50;
-    margin-top: 60px;
+}
+
+header {
+    grid-area: header;
+    background-color: #2c3e50;
+    color: white;
+    padding: 10px;
+}
+
+nav {
+    grid-area: nav;
+    background-color: #44596e;
+    color: white;
+    padding: 10px;
+}
+
+main {
+    grid-area: main;
+    background-color: #ecf0f1;
+    padding: 10px;
+}
+
+@media (min-width: 66rem) {
+    .content {
+        padding-left: calc(10% + 1.5rem);
+        padding-right: 2rem;
+    }
+}
+
+@media (min-width: 99rem) {
+    .content {
+        max-width: 70rem;
+        padding-left: 200px;
+    }
+}
+
+footer {
+    grid-area: footer;
+    background-color: #2c3e50;
+    color: white;
+    padding: 10px;
 }
 </style>
