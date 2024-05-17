65d93ef6d34ee3c0951b3610d82a08490c129045
Neill Robson <neill.robson@pendo.io>
2024-05-17 16:09:19 -0400

    Branding updates

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'Prism Design System'
+        }
+    },
     configureWebpack: {
         module: {
             rules: [
