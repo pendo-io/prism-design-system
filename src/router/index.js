ddb7535ebad04da8c8959ddca814f191d314954a
Neill Robson <neill.robson@pendo.io>
2024-05-20 14:57:54 -0400

    Use at symbol for local imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Index from '../pages/Index.mdx';
-import MyMarkdown from '../pages/components/MyMarkdown.mdx';
+import Index from '@/pages/Index.mdx';
+import MyMarkdown from '@/pages/components/MyMarkdown.mdx';
 
 Vue.use(VueRouter);
 
