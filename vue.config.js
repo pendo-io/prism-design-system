b07393edb8e4967bf79e731d71678072afe7d00a
Neill Robson <neill.robson@pendo.io>
2024-05-20 13:44:50 -0400

    Set base path to prism-design-system

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+    publicPath: '/prism-design-system/',
     transpileDependencies: true,
     pages: {
         index: {
