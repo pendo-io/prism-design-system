3170562c34e83ef67e60d2e65a60cd1df45a9977
Neill Robson <neill.robson@pendo.io>
2024-05-22 17:20:11 -0400

    Automatic route generation unordered

--- src/router/index.js
+++ src/router/index.js
@@ -15,51 +15,6 @@ const routes = [
         component: () => import('@/views/HomePage.vue')
     },
     {
-        path: '/components/buttons',
-        component: () => import('@/content/components/buttons/index.mdx'),
-        children: [
-            {
-                name: 'ButtonsSpecs',
-                path: 'specs',
-                meta: {
-                    prop: 'specs',
-                    label: 'Specs'
-                },
-                component: () => import('@/content/components/buttons/spec.mdx')
-            },
-            {
-                name: 'ButtonsUsage',
-                path: 'usage',
-                meta: {
-                    prop: 'usage',
-                    label: 'Usage examples'
-                },
-                component: () =>
-                    import('@/content/components/buttons/usageExamples.mdx')
-            },
-            {
-                name: 'ButtonsProperties',
-                path: 'properties',
-                meta: {
-                    prop: 'properties',
-                    label: 'Properties'
-                },
-                component: () =>
-                    import('@/content/components/buttons/properties.mdx')
-            },
-            {
-                name: 'ButtonsGuidelines',
-                path: 'guidelines',
-                meta: {
-                    prop: 'guidelines',
-                    label: 'Copy guidelines'
-                },
-                component: () =>
-                    import('@/content/components/buttons/copyGuidelines.mdx')
-            }
-        ]
-    },
-    {
         path: '*',
         component: () => import('@/views/NotFoundPage.vue')
     }
--- src/router/register.js
+++ src/router/register.js
@@ -3,31 +3,79 @@ const requireComponent = require.context('@/content', true, /\.mdx$/);
 
 const toVueRouterPath = (str) =>
     str
-        .replace(/\.\w+$/, '')
-        .replace('index', '')
-        .replace('.', '')
-        .replace(/\/$/, '');
+        .replace(/\.\w+$/, '') // Remove file extension
+        .replace('index', '') // Remove index (filename)
+        .replace('.', '') // Remove other extraneous periods
+        .replace(/\/$/, ''); // Remove trailing slash
 
-const routes = [];
+// #############################################################################
+
+const metaTree = {};
 
 for (const fileName of requireComponent.keys().sort()) {
     const path = toVueRouterPath(fileName);
-    const contentModule = requireComponent(fileName);
-    const newRoute = {
-        path,
-        component: contentModule.default
-    };
+    const component = requireComponent(fileName);
 
-    const parentPath = path.replace(/\/[^/]+$/, '');
-    const parentRoute = routes.find((route) => route.path === parentPath);
-
-    if (contentModule.IS_TAB && parentRoute) {
-        newRoute.path = path.replace(`${parentPath}/`, '');
-        parentRoute.children = parentRoute.children || [];
-        // parentRoute.children.push(newRoute);
-    } else {
-        // routes.push(newRoute);
+    const pathKeys = path.split('/');
+    pathKeys.shift(); // the first item is always empty due to leading slash
+    let current = metaTree;
+    for (const key of pathKeys) {
+        if (!current.subRoutes) {
+            current.subRoutes = {};
+        }
+        if (!current.subRoutes[key]) {
+            current.subRoutes[key] = {};
+        }
+        current = current.subRoutes[key];
     }
+
+    current.component = component;
 }
+
+// #############################################################################
+
+let count = 0;
+
+function createRoute(key, value) {
+    let routes = [];
+
+    if (value.subRoutes) {
+        routes = Object.entries(value.subRoutes).flatMap(([key, value]) =>
+            createRoute(key, value)
+        );
+    }
+
+    const siblings = [];
+    const children = [];
+
+    for (const route of routes) {
+        if (route.meta?.tab) {
+            children.push(route);
+        } else {
+            route.path = `${key}/${route.path}`;
+            siblings.push(route);
+        }
+    }
+
+    if (value.component) {
+        const selfRoute = {
+            name: `${count++}`,
+            path: key || '/',
+            component: value.component.default,
+            meta: value.component.META
+        };
+        if (children.length) {
+            selfRoute.children = children;
+        }
+        siblings.unshift(selfRoute);
+    }
+
+    return siblings;
+}
+
+// #############################################################################
+
+const routes = createRoute('', metaTree);
+console.log(routes);
 
 export default routes;
