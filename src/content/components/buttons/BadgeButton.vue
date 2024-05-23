b304501123859de9568448ecd58724f83c36e575
Neill Robson <neill.robson@pendo.io>
2024-05-23 10:44:22 -0400

    Finish pendo button docs

---
+++ src/content/components/buttons/BadgeButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <pendo-button>
+        <div class="contents">
+            Switch to
+            <pendo-tag class="tag" label="BETA" size="mini" />
+        </div>
+    </pendo-button>
+</template>
+
+<script>
+import { PendoButton, PendoTag } from '@pendo/components';
+
+export default {
+    name: 'BadgeButton',
+    components: {
+        PendoButton,
+        PendoTag
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_colors.scss';
+
+.contents {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+}
+
+.tag {
+    border: 0;
+    background-color: white !important;
+    color: $color-teal-primary !important;
+}
+</style>
---
+++ src/content/components/buttons/IconButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <pendo-button
+        v-pendo-tooltip="'Send message'"
+        type="secondary"
+        icon="send"
+    />
+</template>
+
+<script>
+import { PendoButton, PendoTooltip } from '@pendo/components';
+
+export default {
+    name: 'IconButton',
+    directives: {
+        PendoTooltip
+    },
+    components: {
+        PendoButton
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_colors.scss';
+
+.contents {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+}
+
+.tag {
+    border: 0;
+    background-color: white !important;
+    color: $color-teal-primary !important;
+}
+</style>
