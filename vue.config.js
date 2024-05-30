9ab18e8a5d724a5e50debfd9c7b5eab3ac628951
Neill Robson <neill.robson@pendo.io>
2024-05-30 15:27:00 -0400

    Use relative publicPath for lookasides

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-    publicPath: '/prism-design-system/',
+    publicPath: '',
     transpileDependencies: true,
     pages: {
         index: {
