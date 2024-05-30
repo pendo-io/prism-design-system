901eb4ec7e3b63e85bd364c95d6a4c27aa7eada7
Neill Robson <neill.robson@pendo.io>
2024-05-30 13:57:42 -0400

    First attempt at CircleCI lookaside deployment
    No testing has been done yet. Still need to push this commit to Github
    and open a PR.

---
+++ scripts/lookaside.js
@@ -0,0 +1,53 @@
+import { Storage } from '@google-cloud/storage';
+import path from 'node:path';
+import fs from 'node:fs';
+import read from 'fs-readdir-recursive';
+import minimist from 'minimist';
+import logSymbols from 'log-symbols';
+
+const { branch, keyFilename } = minimist(process.argv.slice(2));
+
+run();
+
+async function run() {
+    const dist = path.resolve(fs.realpathSync(process.cwd()), 'dist');
+    const files = read(dist).map((file) => {
+        return {
+            name: file,
+            path: path.join(dist, file),
+            destination: `${branch}/${file}`
+        };
+    });
+
+    const storage = new Storage({
+        projectId: 'pendo-dev',
+        keyFilename
+    });
+
+    const bucket = storage.bucket('prism-design-system');
+
+    try {
+        // clear out destination folder before uploading new files
+        console.log(logSymbols.info, 'Deleting existing files');
+        await bucket.deleteFiles({ prefix: `${branch}/`, force: true });
+
+        console.log(logSymbols.info, 'Uploading new files');
+        const uploads = files.map((file) => {
+            return bucket.upload(file.path, {
+                destination: file.destination,
+                public: true,
+                gzip: true,
+                resumable: false,
+                metadata: {
+                    cacheControl: 'no-cache'
+                }
+            });
+        });
+
+        await Promise.all(uploads);
+        console.log(logSymbols.success, 'Lookaside deployed');
+    } catch (error) {
+        console.log(logSymbols.error, error);
+        process.exit(1);
+    }
+}
