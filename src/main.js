aecc1b948327224dea529be73248d2ff92f8f919
Neill Robson <neill.robson@pendo.io>
2024-06-07 09:51:03 -0400

    Demonstrate MDXProvider restyling

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from 'vue';
 import router from './router';
+import { MDXProvider } from '@mdx-js/vue';
 import App from './App.vue';
 import './main.scss';
 
 Vue.config.productionTip = false;
 
+const components = {
+    blockquote: (props) => ({
+        render(h) {
+            return h('strong', props);
+        }
+    })
+};
+
 new Vue({
-    render: (h) => h(App),
+    render: (h) => h(MDXProvider, { props: { components } }, [h(App)]),
     router
 }).$mount('#app');
